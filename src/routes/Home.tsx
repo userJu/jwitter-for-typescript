403ffc9262b3f24625184152a50a9ae32db4cdeb
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-03-14 22:43:27 +0900

    order by created time

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,4 @@
-import { collection, query, onSnapshot } from "firebase/firestore";
+import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
 import { useEffect, useState } from "react";
 import Tweet from "../components/Tweet";
@@ -30,35 +30,9 @@ interface IHomeprops {
 
 function Home({ userObj }: IHomeprops) {
   const [tweets, setTweets] = useState<ITweets[]>([]);
-  //
-  // const getTweets = async () => {
-  //   // const docSnap = await getDoc(collection(db, "tweets"));
-
-  //   // if (docSnap.exists()) {
-  //   //   console.log("Document data:", docSnap.data());
-  //   // } else {
-  //   //   console.log("No such document!");
-  //   // }
-  //   // 아무래도 안됨
-
-  //   const querySnapshot = await getDocs(query(collection(db, "tweets")));
-  //   querySnapshot.forEach((doc) => {
-  //     // console.log({ ...doc.data() });
-  //     // console.log(doc.data().tweet);
-  //     const tweetObj = {
-  //       text: doc.data().tweet,
-  //       createdAt: doc.data().createdAt,
-  //       id: doc.id,
-  //       userId: doc.data().creatorId,
-  //       // ...doc.data(),
-  //     };
-  //     setTweets((prev) => [tweetObj, ...prev]);
-  //   });
-  // };
-  //
 
   useEffect(() => {
-    const q = query(collection(db, "tweets"));
+    const q = query(collection(db, "tweets"), orderBy("createdAt", "desc"));
     onSnapshot(q, (querySnapshot) => {
       const tweetArray: any = querySnapshot.docs.map((doc) => ({
         text: doc.data().text,
